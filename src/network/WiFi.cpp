43d864fd4f40bd4267b5cdd292e68c80d1ba5aa7
901b7190 <noreply@example.com>
2022-07-31 00:18:20 +0200

    add mdns service

--- src/network/WiFi.cpp
+++ src/network/WiFi.cpp
@@ -1,15 +1,32 @@
+#include <mdns.h>
 #include <WiFiManager.h>
+
+#include "OSSM_Debug.h"
 
 namespace OSSMStroke {
     namespace Network {
         namespace WiFi {
             WiFiManager wifiManager;
 
+            void setupMdns() {
+                esp_err_t err = mdns_init();
+                if (err) {
+                    LogDebug("Failed to initialize MDNS service.");
+                    return;
+                }
+
+                mdns_hostname_set("ossm-stroke");
+                mdns_service_add(NULL, "_http", "_tcp", 80, NULL, 0);
+                mdns_service_instance_name_set("_http", "_tcp", "OSSM-Stoke HTTP API and WebSocket.");
+            }
+
             void setup() {
                 // reset settings - wipe credentials for testing
                 // wifiManager.resetSettings();
                 wifiManager.setConfigPortalBlocking(false);
                 wifiManager.autoConnect();
+
+                setupMdns();
             }
 
             void loop() {
